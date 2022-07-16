5404caa1d1c85f401c752e0ec1bd55e63c3d03f2
renan <renansilvaytb@gmail.com>
2022-07-16 13:26:33 -0300

    function, arrow function, arguments, parameters

--- 7.javascript/2.funcoes/script.js
+++ 7.javascript/2.funcoes/script.js
@@ -1,1 +1,15 @@
+// função anonima, quando nao tem um nome declarado
 
+const phrase = () => {
+  console.log('Runs anonymous function')
+}
+
+phrase()
+
+//  mais exemplos
+//           () = parameters
+const sum = (x, y) => {
+  console.log(x + y)
+}
+
+sum(5, 2) // ( here ) arguments
