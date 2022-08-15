830fec6be1b539c9b3259721ad3deb3dd903b682
renyzeraa <renansilvaytb@gmail.com>
2022-08-15 14:52:47 -0300

    fix global styles

--- 8.fundamentos-react.js/reactapp/src/styles/global.js
+++ 8.fundamentos-react.js/reactapp/src/styles/global.js
@@ -6,4 +6,11 @@ export const GlobalStyles = styled.div`
     margin: 0;
     padding: 0;
   }
+  max-width: 900px;
+  height: 100vh;
+
+  margin: 0 auto;
+
+  font-family: 'Roboto Slab', serif;
+  -webkit-font-smoothing: antialiased;
 `
