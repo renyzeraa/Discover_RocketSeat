68bb93f50c7ca507ef466d2a3cda4b75b60208cc
renyzeraa <renansilvaytb@gmail.com>
2022-08-15 15:09:39 -0300

    feat card component

---
+++ 8.fundamentos-react.js/reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function Card() {
+  return (
+    <Container>
+      <strong>Renan Leandro da Silva</strong>
+      <small>15:06:32</small>
+    </Container>
+  )
+}
---
+++ 8.fundamentos-react.js/reactapp/src/components/Card/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  background: pink;
+  padding: 24px 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 8px 0;
+  border-radius: 10px;
+`
